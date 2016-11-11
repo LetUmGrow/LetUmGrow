4dd2650b61042fda0ed188add8bce38272470f06
Gabriel L. Viloria <gviloria@hawaii.edu>
2016-11-11 02:40:58 -1000

    Hid Navigation Bar at Landing Page, Uploaded Finalized App Logo, Landing Page Background
    I’m still trying to get the background-image to scale correctly.

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,6 +2,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
+  name: 'Landing_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Landing_Page' });
+  },
+});
+
+FlowRouter.route('/home-page', {
   name: 'Home_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Home_Page' });
