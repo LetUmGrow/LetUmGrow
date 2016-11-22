5efbb0687308113345c675c0c1db9eb1e1c07c9a
Bill Mullen <wlmullen@hawaii.edu>
2016-11-22 10:08:11 -1000

    Real version of species DB scheme

---
+++ app/imports/startup/server/index.js
@@ -0,0 +1,4 @@
+import './accounts.js';
+import './stuff.js';
+import './plants.js';
+import './species.js';
---
+++ app/imports/startup/server/plants.js
@@ -0,0 +1,25 @@
+import {Plants} from '../../api/plants/plants.js';
+import {_} from 'meteor/underscore';
+
+/**
+ * A list of Stuff to pre-fill the Collection.
+ * @type {*[]}
+ */
+const plantSeeds = [
+  {
+    vernacularName: 'Chaulmoogra',
+    scientificName: 'Hydnocarpus anthelmintica',
+    decimalLatitude: '21.297557',
+    decimalLongitude: '-157.820162',
+    plDesc: 'This tree was was originally planted in 1935 by King Prajadhipok of Siam  â€˜ewa of old Farrington Hall, to honor Alice Ball for her work.  When Queen Liliâ€˜uokalani Student Services Center was built, the tree was relocated to its present place.',
+  },
+];
+
+/**
+ * Initialize the Stuff collection if empty with seed data.
+ */
+if (Plants.find().count() === 0) {
+  _.each(plantSeeds, function seedPlants(plant) {
+    Plants.insert(plant);
+  });
+}
