3ca9e70bddce64936551bf5cd01f962db8c9a4f7
matthew-schultz <mschultz@hawaii.edu>
2016-11-25 13:45:40 -1000

    Added google-maps package & associated code
    Executed command 'meteor add dburles:google-maps'
    
    Followed steps 1 and 2 from
    http://meteorcapture.com/how-to-create-a-reactive-google-map/ (Added
    some map styling into style.css)

--- app/imports/ui/pages/plant-map.js
+++ app/imports/ui/pages/plant-map.js
@@ -1,3 +1,45 @@
 /**
  * Created by matthew on 11/25/2016.
  */
+// import { AutoForm } from 'meteor/aldeed:autoform';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+// import { Plants } from '../../api/plants/plants.js';
+
+
+/*
+ * This function breaks the css styling for some reason
+ */
+// Template.contact.onRendered(function() {
+//   GoogleMaps.load();
+// });
+
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    GoogleMaps.load( { key: 'AIzaSyBBkGBcI1a-ZC9e0PsxeOSVOP02IzcjQwo' } );
+  });
+}
+
+Template.Plant_Map_Page.helpers({
+  exampleMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(21.300005,-157.8211567,17),
+        zoom: 16
+      };
+    }
+  }
+});
+
+// Template.Plant_Map_Page.onCreated(function() {
+//   // We can use the `ready` callback to interact with the map API once the map is ready.
+//   GoogleMaps.ready('exampleMap', function(map) {
+//     // Add a marker to the map once it's ready
+//     var marker = new google.maps.Marker({
+//       position: map.options.center,
+//       map: map.instance
+//     });
+//   });
+// });
