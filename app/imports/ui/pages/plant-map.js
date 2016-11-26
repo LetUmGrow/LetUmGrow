f55724c869f9c158587efc00c64df9b36b285acc
matthew-schultz <mschultz@hawaii.edu>
2016-11-26 00:44:28 -1000

    The map now displays points from the database!
    See http://meteorcapture.com/how-to-create-a-reactive-google-map/#step6

--- app/imports/ui/pages/plant-map.js
+++ app/imports/ui/pages/plant-map.js
@@ -40,6 +40,45 @@ Template.Plant_Map_Page.onCreated(function () {
       Plants.insert({ decimalLatitude: event.latLng.lat(), decimalLongitude: event.latLng.lng() });
     });
 
+    var plantMarkers = {};
+
+    Plants.find().observe({
+      added: function(document) {
+        // Create a marker for this document
+        var marker = new google.maps.Marker({
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: new google.maps.LatLng(document.decimalLatitude, document.decimalLongitude),
+          map: map.instance,
+          // We store the document _id on the marker in order
+          // to update the document within the 'dragend' event below.
+          id: document._id
+        });
+
+        // This listener lets us drag plantMarkers on the map and update their corresponding document.
+        google.maps.event.addListener(marker, 'dragend', function(event) {
+          Plants.update(marker.id, { $set: { decimalLatitude: event.latLng.lat(), decimalLongitude: event.latLng.lng() }});
+        });
+
+        // Store this marker instance within the plantMarkers object.
+        plantMarkers[document._id] = marker;
+      },
+      changed: function(newDocument, oldDocument) {
+        plantMarkers[newDocument._id].setPosition({ decimalLatitude: newDocument.decimalLatitude, decimalLongitude: newDocument.decimalLongitude });
+      },
+      removed: function(oldDocument) {
+        // Remove the marker from the map
+        plantMarkers[oldDocument._id].setMap(null);
+
+        // Clear the event listener
+        google.maps.event.clearInstanceListeners(
+            plantMarkers[oldDocument._id]);
+
+        // Remove the reference to this marker instance
+        delete plantMarkers[oldDocument._id];
+      }
+    });
+
     // Add a marker to the map once it's ready
     // var marker = new google.maps.Marker({
     //   position: map.options.center,
