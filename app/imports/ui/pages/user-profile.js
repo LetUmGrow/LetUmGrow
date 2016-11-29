ef0cfcd0650f9bf429b377b75496bbe17f902e87
Bill Mullen <wlmullen@hawaii.edu>
2016-11-28 20:37:59 -1000

    functionality for users and contacts, equivalent to Digits 3 and 4, plus 50% digits 7

--- app/imports/ui/pages/user-profile.js
+++ app/imports/ui/pages/user-profile.js
@@ -2,9 +2,9 @@
  * Created by wlmullen on 11/14/16.
  */
 import { Template } from 'meteor/templating';
-import { Stuff } from '../../api/contacts/contacts.js';
+import { Contacts } from '../../api/contacts/contacts.js';
 
-Template.List_Stuff_Page.helpers({
+Template.User_Profile_Page.helpers({
 
   /**
    * @returns {*} All of the Contacts documents.
@@ -12,4 +12,10 @@ Template.List_Stuff_Page.helpers({
   contactsList() {
     return Contacts.find();
   },
+});
+
+Template.User_Profile_Page.onCreated(function onCreated() {
+  this.autorun(() => {
+    this.subscribe('Contacts');
+  });
 });
