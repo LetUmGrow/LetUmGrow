35ef7a39f277fde7283fbf46a9eabd422d5bdfa2
matthew-schultz <mschultz@hawaii.edu>
2016-12-01 07:40:14 -1000

    Cas login documentation and edits
    Added some login documentation to the CAS files and the landing page
    files
    
    Removed the close window button from the modal, since the decline button
    also closes the window

--- app/imports/ui/layouts/cas-login.js
+++ app/imports/ui/layouts/cas-login.js
@@ -27,10 +27,12 @@ Template.Cas_Login.events({
       }
     };
     Meteor.loginWithCas(callback);
+
     // FlowRouter.go('User_Profile_Page');
-    $('.ui.modal.landing-page')
-        .modal('hide')
-    ;
+
+    //hides modal on landing page; need to test how it works when the user doesn't log in successfully
+    $('.ui.modal.landing-page').modal('hide');
+
     return false;
   },
 });
